d93e05c5391e66a59ae404b62390c41111b41fc3
cliffgor <clierozigy@gmail.com>
2022-06-26 14:37:23 +0300

    The print("") method is a code blog that tells our IDEA to display something

---
+++ src/main/kotlin/Basics.kt
@@ -0,0 +1,7 @@
+// This is how we create a function and the function we created is called
+// Main that is the starting point of the application
+
+fun main() {
+//    The print("") method is a code blog that tells our IDEA to display something
+    print("Hello Kotlin Basics")
+}

777c4a99fbda19f0d65940cdffb73b58fcd12f73
cliffgor <clierozigy@gmail.com>
2022-06-26 14:41:23 +0300

    added a var with the name topic and replaced it on the print statement to read the value inside the var

--- src/main/kotlin/Basics.kt
+++ src/main/kotlin/Basics.kt
@@ -2,6 +2,9 @@
 // Main that is the starting point of the application
 
 fun main() {
+
+    var topic = "We are Learning Kotlin Basics"
+
 //    The print("") method is a code blog that tells our IDEA to display something
-    print("Hello Kotlin Basics")
+    print("Hello " + topic)
 }

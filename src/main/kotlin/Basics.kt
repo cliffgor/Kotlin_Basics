181b56cf087a4e80222cbf6aa402dd4d41447844
cliffgor <clierozigy@gmail.com>
2022-06-27 10:18:03 +0300

    Mutable and Immutable variables explanation var and val

--- src/main/kotlin/Basics.kt
+++ src/main/kotlin/Basics.kt
@@ -2,9 +2,14 @@
 // Main that is the starting point of the application
 
 fun main() {
-
-    var topic = "We are Learning Kotlin Basics"
+// Here we created a var that stores the topic and print as Hello + topic
+// Difference between var and val
+//    Var can be reassigned a new value(Mutable)
+//    val can only be assigned once and the value can not be overwritten (Immutable)
+    //var topic = "We are Learning Kotlin Basics"
+    val topic = "We are Learning Kotlin Basics"
 
 //    The print("") method is a code blog that tells our IDEA to display something
+//    Here we add the variable to the Print Statement
     print("Hello " + topic)
 }

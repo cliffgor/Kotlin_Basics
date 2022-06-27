976c74802df267f7e22b6e228fde612445505f09
cliffgor <clierozigy@gmail.com>
2022-06-27 21:15:29 +0300

    Datatype of number Instead of defining the datatype Kotlin automatically assigns the data type. In a case you have a string it would assign itself the value of a sting if you do not put it inside a paranthsis then it would automatically be an integer

--- src/main/kotlin/Basics.kt
+++ src/main/kotlin/Basics.kt
@@ -9,6 +9,19 @@ fun main() {
     //var topic = "We are Learning Kotlin Basics"
     val topic = "We are Learning Kotlin Basics"
 
+//    types string
+    val myName = "Frank"
+
+//    type int 32 bit
+    var myAge = 31
+
+//Integer TYPES: Byte (8bit), Short(26 bit), Int(32 bit), Long(64 bit)
+
+    val myByte: Byte = 12
+    val myShot: Short = 125
+    val myInt: Int = 123123123
+    val myLong: Long = 39_812_309_487_120_300
+
 //    The print("") method is a code blog that tells our IDEA to display something
 //    Here we add the variable to the Print Statement
     print("Hello " + topic)
